79addc3fc2b7ed1e10c44e717f6fec0f009cfa2b
Mehran Shahidi <m.mehrashahidi@gmail.com>
2025-06-30 07:30:50 +0200

    mason: fix options

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -98,16 +98,13 @@ function M.setup(_, opts)
 	-- get all the servers that are available thourgh mason-lspconfig
 	local have_mason, mlsp = pcall(require, "mason-lspconfig")
 	local all_mslp_servers = {}
-	if have_mason then
-		all_mslp_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
-	end
 
 	local ensure_installed = {} ---@type string[]
 	for server, server_opts in pairs(servers) do
 		if server_opts then
 			server_opts = server_opts == true and {} or server_opts
 			-- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
-			if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
+			if server_opts.mason == false  then
 				setup(server)
 			else
 				ensure_installed[#ensure_installed + 1] = server
@@ -115,10 +112,6 @@ function M.setup(_, opts)
 		end
 	end
 
-	if have_mason then
-		mlsp.setup({ ensure_installed = ensure_installed })
-		mlsp.setup_handlers({ setup })
-	end
 end
 
 return M
